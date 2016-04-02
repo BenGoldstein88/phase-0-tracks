f3810dc9ab9488c6d79463e4c67bf59511d96681
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-01 20:31:21 -0700

    Demonstrate .each for array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,20 @@ def say_goodbye
 end
 
 say_goodbye { |name1, name2| puts "See ya later #{name1} and #{name2}!"}
+
+###
+
+frogs = ["jim", "johnny", "joe", "jason"]
+ages = {jim: 2, johnny: 4, joe: 1, jason: 3}
+
+frogs.each do |name|
+puts "This frog is #{name}."
+end
+
+ages.each do |name, age|
+puts "#{name} is #{age} years old."
+end
+
+
+
+

ae3f39c0fadfbb104fb5245ea8be55226da6b49a
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-01 21:11:47 -0700

    Add release 2 #2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -48,7 +48,7 @@ puts ages
 # Begin release 2
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-hash = {name: "jim", age: 10, state: "NV"}
+hash = {jim: 5, john: 10, joe: 20}
 
 # 1
 
@@ -61,6 +61,43 @@ nums.each do |i|
 end
 
 puts new_nums
+
+
+puts hash
+hash.each do |name, age|
+  if hash[name] < 10
+    hash.delete(name)
+  end
+end
+puts hash
+
+# 2
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+hash = {jim: 5, john: 10, joe: 20}
+new_nums = []
+
+puts new_nums
+
+nums.each do |i|
+  if i > 4
+    new_nums << i
+  end
+end
+
+puts new_nums
+
+new_hash = {}
+puts hash
+puts new_hash
+hash.each do |name, age|
+  if hash[name] >= 10
+    new_hash[name] = age
+  end
+end
+puts hash
+puts new_hash
+
+
 
 
 

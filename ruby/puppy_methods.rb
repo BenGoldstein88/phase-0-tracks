b85f191b097b0a3715b032a56d7f3f22abac4017
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-05 13:36:47 -0700

    Add dog class/methods, complete release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,36 @@ class Puppy
     toy
   end
 
+  def speak(i)
+    x = 0
+    while x < i
+      puts "Woof!"
+      i = i-1
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    dog_age = human_years*7
+    puts dog_age
+    return dog_age
+
+  end
+
+  def play_dead
+    puts "*plays dead*"
+  end
+
+
+
 end
 
+tod = Puppy.new
+tod.fetch('bone')
+tod.speak(5)
+tod.roll_over
+tod.dog_years(5)
+tod.play_dead

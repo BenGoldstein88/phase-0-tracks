41c8e7a295564778a5ef8213343e968f0a352a2f
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-05 13:24:19 -0700

    Add mock-up of puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#Puppy (Class)
+
+#Species: Australian Shepherd
+
+#Characteristics:
+  #fur color: gray
+ # friendly: true
+ # height: 40 in
+ # weight: 100lbs
+ # name: Tod
+
+#Behavior:
+ # run
+ # jump
+ # fetch
+ # sleep

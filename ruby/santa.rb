4e2c0f59643c3d195df3c1c9180db20e1f140bf4
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-08 22:03:46 -0700

    Add santa class, methods, and instance vars

---
+++ ruby/santa.rb
@@ -0,0 +1,27 @@
+class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(type)
+    puts "That was a good #{type}!"
+  end
+
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  end
+
+  def speak
+    puts "I am a #{@gender} #{@ethnicity}, am #{@age} years old, and prefer #{@reindeer_ranking}."
+  end
+
+end
+
+bill = Santa.new("male", "Pacific Islander")
+bill.speak
+

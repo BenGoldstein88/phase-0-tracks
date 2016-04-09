4a86366210c883bb7fcd40b6be0e296fc96f297e
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-08 22:19:49 -0700

    Add getters, new driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -20,8 +20,51 @@ class Santa
     puts "I am a #{@gender} #{@ethnicity}, am #{@age} years old, and prefer #{@reindeer_ranking}."
   end
 
+  def celebrate_birthday
+    @age = @age + 1
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << reindeer
+  end
+
+  # getters
+
+  def gender
+    @gender
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def reindeer_ranking
+    @reindeer_ranking
+  end
+
 end
 
-bill = Santa.new("male", "Pacific Islander")
-bill.speak
+bill = Santa.new("female", "asian")
+puts bill.gender
+puts bill.ethnicity
+puts bill.reindeer_ranking
+bill.get_mad_at("Rudolph")
+puts bill.reindeer_ranking
 
+
+
+
+
+
+=begin
+santas = []
+genders = ["male", "female", "trans", "hermaphroditic", "ambiguous", "other"]
+ethnicities = ["asian", "other asian", "southeast asian", "russian asian", "former soviet bloc asian", "secret asian"]
+
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicities[i])
+end
+
+puts santas
+=end

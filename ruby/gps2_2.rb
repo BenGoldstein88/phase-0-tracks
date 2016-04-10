5b3418ad9932fc4ce7ccc16bef1147ca1fbc7ad7
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-10 08:14:28 -0700

    Complete gps, add grocery list methods and tests, answer reflection questions

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,1 +1,129 @@
-# test code
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # Make empty hash
+  # Split string using split method with white space as a delimiter
+# FOR each new string
+  # Pull first (new) string into hash
+  # set default quantity
+  # repeat for each (new) string
+# end loop when out of strings
+  # print the list to the console (using print method below)
+# output: hash
+
+
+def create_list(list)
+  groceries = {}
+  list_arr = list.split(' ')
+
+  list_arr.each do |item|
+    groceries[item] = 1
+  end
+
+
+  # print_list(groceries)
+  return groceries
+
+
+end
+
+# Method to add an item to a list
+# input: item name and optional quantity, hash to add item to
+# steps:
+  #  assign key of hash to input (item name)
+  # IF quantity
+    # assign value to quantity
+  # ELSE set quantity to 1
+# output: return updated hash
+
+# this is inelegant, use splat??
+def add_item(item, hash)
+  if hash.include?(item)
+    return hash
+  else
+  hash[item] = 1
+  return hash
+end
+
+
+def add_item(item, quantity, hash)
+  if hash.include?(item)
+    update_quantity(item, quantity, hash)
+    return hash
+  else
+    hash[item] = quantity
+    return hash
+  end
+
+end
+
+
+# Method to remove an item from the list
+# input: item name and hash
+# steps:
+  # remove input from hash (and associated value)
+# output: return updated hash
+
+def remove_item(item, hash)
+  hash.delete(item)
+  return hash
+end
+
+# Method to update the quantity of an item
+# input: item name, hash, and quantity
+# steps:
+  # access value pair @ name => old-quantity
+  # set value @ name to new-quantity
+# output: return updated hash
+
+def update_quantity(item, quantity, hash)
+  hash[item] = quantity
+  return hash
+end
+
+# Method to print a list and make it look pretty
+# input: hash
+# steps:
+  # loop the hash
+  # print key value pair for each key, format with puts and stuff
+  #  end loop
+# output: list of hash contents (long string?){no real return value}
+
+def print_list(hash)
+  puts "Here is this week's grocery list:"
+  hash.each do |item, quantity|
+    puts "#{item}: #{quantity}"
+  end
+end
+
+
+bill = create_list("carrots apples cereal pizza")
+
+print_list(bill)
+add_item('Lemonade', 2, bill)
+add_item('Tomatoes', 3, bill)
+add_item('Onions', 1, bill)
+add_item('Ice Cream', 4, bill)
+
+print_list(bill)
+
+remove_item('Lemonade', bill)
+
+print_list(bill)
+
+update_quantity('Ice Cream', 1, bill)
+
+
+
+=begin
+
+1. Pseudocode should be more specific than i thought.
+2. Hashes make more sense. Arrays would have to be nested (e.g. an array of arrays, where each interior array had two values - item and quantity)
+3. A method returns its return value.
+4. Any type of data/variable can be a method argument
+5. You can pass info between methods by using the output (return) of one as the input for another.
+6. Pseudocode specificity was locked in. Still unclear on how to deal with potentially empty parameters (see add_item method(s) above)
+
+
+
+=end

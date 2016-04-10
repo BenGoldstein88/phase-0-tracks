8b4628efed6335aa05be3c3e5746fc55e91abcde
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-09 20:40:43 -0700

    Change shout to mixin, add sample classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
   def self.yell_angrily(words)
@@ -15,3 +16,33 @@ end
 puts Shout.yell_angrily("Hey")
 
 puts Shout.yell_angrily("Hello")
+=end
+
+module Shout
+
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+
+  def yell_happily(words)
+    words + "!!"  + " :)"
+  end
+
+end
+
+class Boy
+  include Shout
+end
+
+class Girl
+  include Shout
+end
+
+boy = Boy.new
+puts boy.yell_happily("Hey")
+puts boy.yell_angrily("What")
+
+girl = Girl.new
+puts boy.yell_happily("Bonjour")
+puts girl.yell_angrily("Quoi")

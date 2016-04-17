bfc1d92dc61d7b032f00ea5ef8e30e5e387526f4
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-17 11:27:20 -0700

    Add code to assign colors to horses and store result in an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,12 @@ var names = ["Ed", "Barnabas", "Highwayman", "Scrimbles"];
 
 colors.push("yellow-ochre");
 names.push("Matilda");
+
+var horses = {}
+
+for (var i = 0; i < colors.length; i++){
+  horses[names[i]] = colors[i];
+
+}
+
+console.log(horses)

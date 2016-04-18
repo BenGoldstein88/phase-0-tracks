b237088bddd5e4bd1892ce1ff3cd0038a044482a
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-04-17 18:13:43 -0700

    Add function that takes an integer that makes random strings of random length that many times, driver code, comments, finish release 2

--- js/algos.js
+++ js/algos.js
@@ -23,7 +23,7 @@ function longestWord(words) {
     }
   }
 
-console.log(currentLongest);
+
 return currentLongest;
 }
 
@@ -54,10 +54,48 @@ function compareKeyValue(obj1, obj2) {
 }
 
 
+//Pseudocode:
+// create string of possible characters and container array
+// FOR each time in parameter, pick a random string length with Math.random()
+// - then fill that string with random characters using Math.random and characters string
+// push the string to the container array
+// return container array
+
+function randomData(length) {
+  // characters string to pick randomly from
+  var characters = 'abcdejghijklmnopqrstuvwxyz'
+  // container array for randomized strings
+  var strings = [];
+
+
+  // make random words 'length' times
+  for (var i = 0; i < length; i++){
+    var currentString = "";
+
+    // pick random length of word 1-10 letters
+    var currentStringLength = Math.floor((Math.random() * 10) + 1);
+
+    // for each letter in the random word
+    for (var j = 0; j < currentStringLength; j++){
+      // pick a random character and concat. to currentString
+      var character = characters[Math.floor(Math.random()*characters.length)];
+      currentString += character;
+
+    }
+    // add current string to string array
+    strings.push(currentString);
+  }
+  // return array of strings
+
+  return strings;
+}
+
+
+
 // driver code
 var test = ["long phrase","longest phrase","longer phrase"];
 
-longestWord(test);
+console.log(longestWord(test));
 
 // driver code
 var object1 = {name: "Bob", age: 50};
@@ -67,3 +105,12 @@ var object4 = {ponies: "Karl", ripening: 12};
 
 console.log(compareKeyValue(object1, object2));
 console.log(compareKeyValue(object3, object4));
+
+// driver code
+// 10 times: make an array of random length 1-10, find the longest word, print the result
+var times = 10
+for (var i = 0; i < times; i++){
+  var data = randomData(Math.floor((Math.random() * 10) + 1));
+  var result = longestWord(data);
+  console.log(result);
+}
